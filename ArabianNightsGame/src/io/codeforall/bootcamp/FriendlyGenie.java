5eb7d60ec73659a2f1a0cafbc0ca9231b0dc1cea
gustavoParaiso <gustavoclfp@gmail.com>
2025-02-17 09:39:24 +0000

    Adicionadas alterações de ArabianNightsGame

--- ArabianNightsGame/src/io/codeforall/bootcamp/FriendlyGenie.java
+++ ArabianNightsGame/src/io/codeforall/bootcamp/FriendlyGenie.java
@@ -3,6 +3,6 @@ package io.codeforall.bootcamp;
 public class GoodGenie extends Genie {
     public GoodGenie(int numberOfWishes){
         super(numberOfWishes);
-        System.out.println(name + ", the good genie, appeared");
+        System.out.println(getName() + ", the good genie, appeared");
     }
 }
---
+++ ArabianNightsGame/src/io/codeforall/bootcamp/Playground.java
@@ -0,0 +1,12 @@
+package io.codeforall.bootcamp;
+
+public class SandBox {
+    public static void main(String[] args) {
+        Lamp lamp = new Lamp(0);
+        Lamp myLittleLamp = new Lamp(3);
+        Genie genie1 = lamp.rub(2);
+        genie1.grantAWish("Fast car");
+        lamp.recharge(genie1);
+
+    }
+}
